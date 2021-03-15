431b85313cb475217d04adea2406e3c8dff7f26e
pietrodito <pietrodito@gmail.com>
2021-03-15 12:54:00 +0100

    Mute create_ordered_scripts during tests.

--- tests/testthat/test-create_ordered_script.R
+++ tests/testthat/test-create_ordered_script.R
@@ -1,12 +1,13 @@
 test_that("create_ordered_script works", {
 
-  create_ordered_script("data-management", "temp/create-1-3")
+  qcos <- purrr::quietly(create_ordered_script)
+  qcos("data-management", "temp/create-1-3")
 
   check_file_numbers("temp/create-1-3", 1:3)
   
-  create_ordered_script("import", "temp/empty")
+  qcos("import", "temp/empty")
   check_file_numbers("temp/empty", 1)
  
-  create_ordered_script("data-management", "temp/empty")
+  qcos("data-management", "temp/empty")
   check_file_numbers("temp/empty", 1:2)
 })
