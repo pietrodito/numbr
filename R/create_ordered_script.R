4ccf3615030f1ca6c5d3e536d685568fb4280465
pietrodito <pietrodito@gmail.com>
2021-03-16 10:56:49 +0100

    Open file after creation with create_ordered_script()

--- R/create_ordered_script.R
+++ R/create_ordered_script.R
@@ -1,5 +1,6 @@
+#' Create a new script 
 #' @export
-create_ordered_script <- function(name, path = "R") {
+create_ordered_script <- function(name, path = "R", edit_file = T) {
   nbs <- list_r_numbered_files(path)$numbers
 
   first_gap <- 0
@@ -14,7 +15,10 @@ create_ordered_script <- function(name, path = "R") {
       first_gap <- min(discordant)
   }
   new_file <- paste0(path, "/", first_gap, "-", name, ".R")
-    fs::file_create(new_file)
-    add_leading_zero(path)
-    message(paste0("File created: ", new_file))
+  
+  fs::file_create(new_file)
+  add_leading_zero(path)
+  message(paste0("File created: ", new_file))
+    
+  if(edit_file) file.edit(new_file)  
 }
