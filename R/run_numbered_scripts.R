82f757e5a05b7e1b3f648795c928de25ed7aa361
pietrodito <pietrodito@gmail.com>
2021-03-17 10:33:35 +0100

    Export and document run_numbered_scripts().

--- R/run_numbered_scripts.R
+++ R/run_numbered_scripts.R
@@ -1,3 +1,8 @@
+#' Run all numbered scripts in the target directory.
+#' 
+#' @param path (chr) The target directory (default = "R")
+#' 
+#' @export 
 run_numbered_scripts <- function(path = "R") {
   purrr::walk(list_r_numbered_files(path), source)
 }
