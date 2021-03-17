3b5e3baf1637ddf6ef3b2acf2c2ff88cee15ba22
pietrodito <pietrodito@gmail.com>
2021-03-17 11:00:37 +0100

    Save all documents when run_nb_scripts() is called

--- R/run_numbered_scripts.R
+++ R/run_numbered_scripts.R
@@ -1,9 +1,11 @@
 #' Run all numbered scripts in the target directory.
-#' 
+#'
 #' @param path (chr) The target directory (default = "R")
-#' 
-#' @export 
+#'
+#' @export
 run_numbered_scripts <- function(path = "R") {
+  if (Sys.getenv("RSTUDIO") == "1")
+    rstudioapi::documentSaveAll()
   purrr::walk(list_r_numbered_files(path)$files,
-              ~ source(paste0(path, "/", . )))
+              ~ source(paste0(path, "/", .)))
 }
