122a3db7fef58f4789649d23dfd84565446eec52
pietrodito <pietrodito@gmail.com>
2021-03-21 17:17:37 +0100

    Fix namespace issue with utils::tail

--- R/nbr_source_all.R
+++ R/nbr_source_all.R
@@ -8,5 +8,5 @@ nbr_source_all <- function(dir = "R", confirm_duration = 10*1e3) {
   if (Sys.getenv("RSTUDIO") == "1")
     rstudioapi::documentSaveAll()
   scripts <-list_r_numbered_files(dir)$files 
-  nbr_source(tail(scripts, n = 1), dir, confirm_duration = confirm_duration)
+  nbr_source(utils::tail(scripts, n = 1), dir, confirm_duration = confirm_duration)
 }
